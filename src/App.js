6c5fc4dcade4aea52e5e84e288dc5fca9da55220
Victor Okonkwo <emmyvic98@gmail.com>
2022-09-16 00:59:41 +0100

    added remove item

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React, { useEffect, useState } from "react";
 import Card from "./components/Card";
 
-const url = "https://course-api.com/react-tours-project"
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  const [data, setData] = useState([]);
+  const [tours, setData] = useState([]);
 
-  const getTours = () =>{
-    fetch(url).then(response => response.json()).then(tours => {
-      // console.log(tours)
-      setData(tours)
-    }).catch(error => console.log(error))
-  }
+  const getTours = () => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((tours) => {
+        // console.log(tours)
+        setData(tours);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const removeItem = (id) => {
+    const newTourList = tours.filter(tour => tour.id !== id)
+    setData(newTourList)
+  };
 
   useEffect(() => {
-    getTours()
-  },[])
+    getTours();
+  }, []);
   return (
     <div className="flex-center-column">
       <div className="container-box">
         <h1>Our Tours</h1>
         <div className="underline"></div>
-        <Card />
+        {tours.map((tour) => {
+          return (
+            <Card key={tour.id} tour={tour}>
+              <button className="btn" onClick={() => removeItem(tour.id)}>
+                Not Interested
+              </button>
+            </Card>
+          );
+        })}
       </div>
     </div>
   );
