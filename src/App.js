6d5d3bf5a3296faac9cf00531d23a4499bf270e1
Victor Okonkwo <emmyvic98@gmail.com>
2022-09-16 01:06:03 +0100

    added isLoading and isError state with conditional rendering

--- src/App.js
+++ src/App.js
@@ -3,26 +3,55 @@ import Card from "./components/Card";
 
 const url = "https://course-api.com/react-tours-project";
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
   const [tours, setData] = useState([]);
 
   const getTours = () => {
     fetch(url)
-      .then((response) => response.json())
+      .then((response) => {
+        if(response.status >= 200  && response.status <= 299){
+          return response.json()
+        }else{
+          setIsError(true)
+          setIsLoading(false)
+          throw new Error('Error ecountered in api')
+        }
+      })
       .then((tours) => {
         // console.log(tours)
         setData(tours);
+        setIsLoading(false)
       })
       .catch((error) => console.log(error));
   };
 
   const removeItem = (id) => {
-    const newTourList = tours.filter(tour => tour.id !== id)
-    setData(newTourList)
+    const newTourList = tours.filter((tour) => tour.id !== id);
+    setData(newTourList);
   };
 
   useEffect(() => {
     getTours();
   }, []);
+  if (isLoading) {
+    return (
+      <div className="flex-center-column">
+        <div className="container-box">
+          <h1>Loading...</h1>
+        </div>
+      </div>
+    );
+  }
+  if (isError) {
+    return (
+      <div className="flex-center-column">
+        <div className="container-box">
+          <h1>Error fetching api</h1>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="flex-center-column">
       <div className="container-box">
